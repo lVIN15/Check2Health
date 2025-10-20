c8060a22e47b4c720b30bf423908e173a67f4b3e
DeeTryo <jrcastillo.tjc@gmail.com>
2025-10-20 20:57:36 +0800

    first commit

---
+++ src/main/java/com/appdev/lastico/castillog3/Castillog3Application.java
@@ -0,0 +1,13 @@
+package com.appdev.lastico.castillog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Castillog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Castillog3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/lastico/castillog3/Castillog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.lastico.castillog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Castillog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
